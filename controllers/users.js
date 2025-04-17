7cf81a6f8402ac6d43cd6060af2da2502ef0e6cf
Raul Jiminian <raul.e.jiminian@gmail.com>
2025-04-17 13:42:06 -0400

    auth and middleware added

--- controllers/users.js
+++ controllers/users.js
@@ -3,28 +3,32 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const signToken = (req, res) => {
-  const user = {
-    _id: 1,
-    username: "rara",
-    password: "rara",
-  };
+export const getUsers = async (req, res) => {
+  try {
+    // Get a list of all users, but only return their username and _id
+    const users = await User.find({}, "username");
 
-  const token = jwt.sign({ user }, process.env.JWT_SECRET);
-  console.log(token);
-
-  res.json({ token });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
 };
 
-export const verifyToken = (req, res) => {
+export const getUser = async (req, res) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    if (req.user._id !== req.params.userId) {
+      return res.status(403).json({ err: "Unauthorized" });
+    }
 
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(req.params.userId);
 
-    res.json({ decode });
-  } catch (error) {
-    res.status(401).json({ err: "Invalid token." });
+    if (!user) {
+      return res.status(404).json({ err: "User not found." });
+    }
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
   }
 };
 
