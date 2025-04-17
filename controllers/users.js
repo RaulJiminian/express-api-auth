13610222b6871e961c4b1bc0fedbe11d533bdb47
Raul Jiminian <raul.e.jiminian@gmail.com>
2025-04-17 11:15:06 -0400

    set up user signup route

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,31 @@
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+
 export const signToken = (req, res) => {
-  res.json({ message: "You are authorized!" });
+  const user = {
+    _id: 1,
+    username: "rara",
+    password: "rara",
+  };
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET);
+  console.log(token);
+
+  res.json({ token });
+};
+
+export const verifyToken = (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    res.json({ decode });
+  } catch (error) {
+    res.status(401).json({ err: "Invalid token." });
+  }
+};
+
+export const signUp = (req, res) => {
+  res.json({ message: "Sign up route" });
 };
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ import * as controllers from "../controllers/users.js";
 const router = Router();
 
 router.get("/sign-token", controllers.signToken);
+router.post('/verify-token', controllers.verifyToken);
+router.post('/sign-up', controllers.signUp);
 
 export default router;
